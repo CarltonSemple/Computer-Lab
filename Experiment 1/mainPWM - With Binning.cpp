b22c0c50ff8cf984bce690c4a9d7a9894be1aec1
Carlton Semple <CarltonSemple@users.noreply.github.com>
2015-02-01 03:28:01 -0500

    added comments

--- Experiment 1/mainPWM - With Binning.cpp
+++ Experiment 1/mainPWM - With Binning.cpp
@@ -1,12 +1,13 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 
-void main(void) {
+void main(void) 
+{
 	// Setup ---------------------------
-     ATD0CTL1_SRES0= 0;
+     ATD0CTL1_SRES0= 0;	
      ATD0CTL1_SRES1= 0;
-     ATD0CTL3_DJM= 1;
-     DDRT=0xFF;
+     ATD0CTL3_DJM= 1;		// right justification of data
+     DDRT=0xFF;				// change Port T's direction to output
      PWME_PWME0=1;
      PWMPOL_PPOL0=1;
      PWMCLK_PCLK0=0; // select clock A
@@ -17,35 +18,23 @@ void main(void) {
     
     int counter=0;
     
-    while(1)
-    {
-      int i=0;
-      int temp;
-      for(i=0; i<256; i++)
-      {
-        ATD0CTL5=0x00;      
-         
-        while(!ATD0STAT0_SCF){}  // Receive data
-        temp += ATD0DR0L;        // add to running total
-          
-      }
-      temp=temp/256;             // get average
-       
-      PTT=temp;                  // send to Port T, which goes to FPGA's 7-segment display
-      
-      // Scale, 0 to 255
-      PWMDTY0=PTT/3 + 25;        // send scaled value to servo motor
-      counter++;                        
-    } 
-     
-
-
-
-  
-  
-  
-  
-  
-  
-  /* please make sure that you never leave main */
+	while(1)
+	{
+		int i=0;
+		int temp;
+		for(i=0; i<256; i++)
+		{
+			ATD0CTL5=0x00			 // Scan from 1 channel and perform a single conversion sequence
+			 
+			while(!ATD0STAT0_SCF){}  // Wait for the Sequence Complete Flag
+			temp += ATD0DR0L;        // Add received data to running total
+		}
+			temp=temp/256;             // Get the average sampled value
+			
+			PTT=temp;                  // send to Port T, which goes to FPGA's 7-segment display
+			
+			// Scale, 0 to 255
+			PWMDTY0=PTT/3 + 25;        // send scaled value to servo motor
+			counter++;                        
+		} 
 }
